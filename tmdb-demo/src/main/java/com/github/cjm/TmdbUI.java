9de1135c499fe9cddb01fa38dfd9d79cd040cabd
Carl J. Mosca <carljmosca@gmail.com>
2015-10-17 22:01:19 -0400

    added filter

--- tmdb-demo/src/main/java/com/github/cjm/TmdbUI.java
+++ tmdb-demo/src/main/java/com/github/cjm/TmdbUI.java
@@ -12,10 +12,12 @@ import com.github.cjm.resource.TvShowCollection;
 import com.github.cjm.service.TvShowService;
 import com.vaadin.annotations.Theme;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.data.util.filter.SimpleStringFilter;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.Grid.HeaderRow;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
@@ -62,22 +64,22 @@ public class TmdbUI extends UI {
         addButtons();
         addUserGrid();
         addMovieGrid();
-        
+
         mainLayout.setExpandRatio(userGridLayout, 1);
         mainLayout.setExpandRatio(movieGridLayout, 2);
 
         setContent(mainLayout);
     }
-    
+
     private void addButtons() {
         buttonLayout.setSpacing(true);
         btnAddUser = new Button("Add User");
         btnAddFavorite = new Button("Add Favorite");
         buttonLayout.addComponent(btnAddUser);
         buttonLayout.addComponent(btnAddFavorite);
-        mainLayout.addComponent(buttonLayout);        
+        mainLayout.addComponent(buttonLayout);
     }
-    
+
     private void addUserGrid() {
         userGridLayout.setSpacing(true);
         userGridLayout.setWidth("100%");
@@ -100,7 +102,7 @@ public class TmdbUI extends UI {
             users.addBean(user);
         }
         userGridLayout.addComponent(fieldLayout);
-        userGridLayout.addComponent(gridLayout);  
+        userGridLayout.addComponent(gridLayout);
         userGridLayout.setExpandRatio(fieldLayout, 1);
         userGridLayout.setExpandRatio(gridLayout, 3);
         mainLayout.addComponent(userGridLayout);
@@ -116,10 +118,35 @@ public class TmdbUI extends UI {
         movieGrid.setColumns("name", "popularity", "voteAverage", "firstAirDate");
         movieGrid.setHeaderVisible(true);
         movieGrid.setContainerDataSource(tvShows);
-        movieGrid.appendHeaderRow();
+        HeaderRow filterRow = movieGrid.appendHeaderRow();
+        // Not really loading "all"; multiple pages up to some reasonable/polite limit (API is throttled and this is a demo afterall).
         tvShows.addAll(tvShowService.loadAll(TvShowService.RESOURCE_TV_POPULAR, TvShowCollection.class).getResults());
         movieGridLayout.addComponent(movieGrid);
         mainLayout.addComponent(movieGridLayout);
+
+        movieGrid.getContainerDataSource()
+                .getContainerPropertyIds().stream().forEach((pid) -> {
+                    if (movieGrid.getColumn(pid) != null) {
+                        Grid.HeaderCell cell = filterRow.getCell(pid);
+
+                        TextField filterField = new TextField();
+                        filterField.setColumns(8);
+                        // Update filter When the filter input is changed
+                        filterField.addTextChangeListener(change -> {
+                            // Can't modify filters so need to replace
+                            tvShows.removeContainerFilters(pid);
+                            // (Re)create the filter if necessary
+                            if (!change.getText().isEmpty()) {
+                                tvShows.addContainerFilter(
+                                        new SimpleStringFilter(pid,
+                                                change.getText(), true, false));
+                            }
+                        });
+                        filterField.setImmediate(true);
+                        cell.setComponent(filterField);
+
+                    }
+                });
 
     }
 }
