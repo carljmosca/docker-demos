b61c3a2f874450cf57d00617c03618bdfcad28d1
Carl J. Mosca <carljmosca@gmail.com>
2015-11-01 21:38:19 -0500

    modified startup scripts

--- tmdb-demo/db/run-client.sh
+++ tmdb-demo/db/run-client.sh
@@ -1,1 +1,1 @@
-docker run -it --link tmdb-db:mysql --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"' tmdb-db-client
+docker run -it --link tmdb-db:mysql --name=tmdb-mysql-client --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"' tmdb-db-client
